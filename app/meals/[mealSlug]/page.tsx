702d4061de98c5407183fb90912bdac74fb1613e
kimsiahooi <kimsiahooi@gmail.com>
2025-10-31 08:54:29 +0800

    ui: enhance

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default async function MealDetailsPage({
   params,
 }: {
-  params: { mealSlug: string };
+  params: Promise<{ mealSlug: string }>;
 }) {
-  const meal = await getMeal(params.mealSlug);
+  const { mealSlug } = await params;
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image
+            fill
+            src={meal.image}
+            alt={meal.title}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            loading="eager"
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>SUMMARY</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
-          dangerouslySetInnerHTML={{ __html: "..." }}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
         />
       </main>
     </>
--- lib/meals.ts
+++ lib/meals.ts
@@ -7,9 +7,11 @@ export const getMeals = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   // throw new Error("Loading meals failed");
-  return <Meal[]>db.prepare("SELECT * FROM meals").all();
+  return <Meal[] | undefined>db.prepare("SELECT * FROM meals").all();
 };
 
-export const getMeal = async (slug: string) => {
-  return <Meal>db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+export const getMeal = (slug: string) => {
+  return <Meal | undefined>(
+    db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug)
+  );
 };
